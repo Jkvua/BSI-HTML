f94906328a262a89ff9fcee5674a3743c2bfe984
Camila Ferreira de Almeida <mila.e.ferreira28@gmail.com>
2024-03-15 10:51:51 -0300

    commit exercicio 2

--- lista01-java/exercicio2/exercicio2.js
+++ lista01-java/exercicio2/exercicio2.js
@@ -1,14 +1,10 @@
 var propostaRecebida = prompt("Marcos informe a proposta que você recebeu pelo ingresso: ");
-var sonoMulher = prompt("Informe o tempo de sono da garota durante o filme: ");
 
-sonoHomem = parseInt(sonoHomem);
-sonoMulher = parseInt(sonoMulher); /*converter para numeros */
+propostaRecebida = parseInt(propostaRecebida)
 
-filmeRomance = 90;
-filmeInvestigacao = 110;
+valorIngresso =  150;
 
-var tempoAcordadoGaroto = filmeRomance - sonoHomem;
-var tempoAcordaddoGarota = filmeInvestigacao - sonoMulher
+var calculoDiferenca = valorIngresso - propostaRecebida;
 
 alert("O tempo que o homem assistiu o filme foi de " + tempoAcordadoGaroto + " minutos");
 alert("O tempo que a mulher ficou acordada assistindo o filme foi de " + tempoAcordaddoGarota + " minutos");
