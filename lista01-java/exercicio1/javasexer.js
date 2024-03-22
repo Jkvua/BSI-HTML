28471e6f43e8b0608c68729219158879886204a7
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-03-22 15:00:41 -0300

    terminando os exercicios da primeira lista

--- lista01-java/exercicio1/javasexer.js
+++ lista01-java/exercicio1/javasexer.js
@@ -6,17 +6,22 @@ acordado o tempo inteiro. Escreva um algoritmo que solicite ao usuário o tempo 
 sono de cada pessoa, depois calcule e mostre na tela quanto tempo cada um ficou
 acordado assistindo os filmes.*/
 
-var sonoHomem = prompt("Informe o tempo de sono do garoto durante o filme: ");
-var sonoMulher = prompt("Informe o tempo de sono da garota durante o filme: ");
-
-sonoHomem = parseInt(sonoHomem);
-sonoMulher = parseInt(sonoMulher); /*converter para numeros */
-
-filmeRomance = 90;
-filmeInvestigacao = 110;
-
-var tempoAcordadoGaroto = filmeRomance - sonoHomem;
-var tempoAcordaddoGarota = filmeInvestigacao - sonoMulher
-
-alert("O tempo que o homem assistiu o filme foi de " + tempoAcordadoGaroto + " minutos");
-alert("O tempo que a mulher ficou acordada assistindo o filme foi de " + tempoAcordaddoGarota + " minutos");
+document.getElementById('calcular').addEventListener('click', function() {
+    var tempoGaroto = parseInt(document.getElementById('tempoGaroto').value);
+    var tempoGarota = parseInt(document.getElementById('tempoGarota').value);
+  
+    var duracaoRomantico = 90;
+    var duracaoInvestigacao = 110;
+  
+    var resultadoGarotoRomantico = duracaoRomantico - tempoGaroto;
+    var resultadoGarotaRomantico = duracaoRomantico;
+    var resultadoGarotoInvestigacao = duracaoInvestigacao;
+    var resultadoGarotaInvestigacao = duracaoInvestigacao - tempoGarota;
+  
+    document.getElementById('resultadoGarotoRomantico').innerText = "Tempo acordado do garoto durante o filme romântico: " + resultadoGarotoRomantico + " minutos";
+    document.getElementById('resultadoGarotaRomantico').innerText = "Tempo acordado da garota durante o filme romântico: " + resultadoGarotaRomantico + " minutos";
+    document.getElementById('resultadoGarotoInvestigacao').innerText = "Tempo acordado do garoto durante o filme de investigação: " + resultadoGarotoInvestigacao + " minutos";
+    document.getElementById('resultadoGarotaInvestigacao').innerText = "Tempo acordado da garota durante o filme de investigação: " + resultadoGarotaInvestigacao + " minutos";
+  
+    document.querySelector('.resultado').style.display = 'block';
+  });
--- lista01-java/exercicio2/exercicio2.js
+++ lista01-java/exercicio2/exercicio2.js
@@ -1,4 +1,17 @@
-var propostaRecebida = prompt("Marcos informe a proposta que você recebeu pelo ingresso: ");
+document.getElementById('calcular').addEventListener('click', function(){
+    var propostaRecebida = parseInt(document.getElementById('propostaRecebida').value);
+
+    var valorIngresso = 150;
+    var calculoDiferenca = valorIngresso - propostaRecebida;
+
+    document.getElementById('calculoDiferenca').innerText = "A diferença entre a proposta recebida e o valor pago pelo ingresso é de: " + calculoDiferenca + 'R$';
+    
+    document.querySelector('.resultado').style.display = 'block';
+});
+
+
+
+/*var propostaRecebida = prompt("Marcos informe a proposta que você recebeu pelo ingresso: ");
 
 propostaRecebida = parseInt(propostaRecebida)
 
@@ -7,4 +20,4 @@ valorIngresso =  150;
 var calculoDiferenca = valorIngresso - propostaRecebida;
 
 alert("O tempo que o homem assistiu o filme foi de " + tempoAcordadoGaroto + " minutos");
-alert("O tempo que a mulher ficou acordada assistindo o filme foi de " + tempoAcordaddoGarota + " minutos");
+alert("O tempo que a mulher ficou acordada assistindo o filme foi de " + tempoAcordaddoGarota + " minutos");*/
